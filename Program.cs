6943013753dab96dc64f3c799d065a512b9d61e0
phjang0408 <phjang0408@naver.com>
2026-03-10 20:24:33 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        float a = 1.55f;
+        int b = (int)a;
+        Console.Write("dffd\n");
+    }
+}
