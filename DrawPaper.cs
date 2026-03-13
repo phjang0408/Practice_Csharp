d976211af23c8c284320335d5b74bd02e503e93e
phjang0408 <phjang0408@naver.com>
2026-03-13 14:21:41 +0900

    0313 공부내역

---
+++ DrawPaper.cs
@@ -0,0 +1,28 @@
+﻿namespace Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 128;   // 1000_0000
+            sbyte b = (sbyte)a;
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+
+            byte c = 255;
+            sbyte d = (sbyte)c;
+            System.Console.WriteLine(c);
+            System.Console.WriteLine(d);
+
+            uint e = uint.MaxValue; // uint형 최대값 4294967295
+            Console.WriteLine(e);
+            e = e + 1;              // 0000 0000 … … 0000
+            Console.WriteLine(e);
+            
+            int f = int.MaxValue;   // int형 최대값 2147483647
+            Console.WriteLine(f);
+            f = f + 1;              // 1000 0000 … 0000
+            Console.WriteLine(f);
+        }
+    }
+}
