ce18aed2b407be514fbe11639b5480163a47e912
phjang0408 <phjang0408@naver.com>
2026-03-20 11:51:21 +0900

    충돌 수정

--- DrawPaper.cs
+++ DrawPaper.cs
@@ -1,28 +1,23 @@
-﻿namespace Drawing
+﻿namespace DrawingPaper
 {
+    class CSVar
+    {
+        int globalVar;
+        const int MAX = 1024;
+        public void Method1()
+        {
+            int localVar;
+            localVar = 100;
+            System.Console.WriteLine(localVar);
+            System.Console.WriteLine(globalVar);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            byte a = 128;   // 1000_0000
-            sbyte b = (sbyte)a;
-            System.Console.WriteLine(a);
-            System.Console.WriteLine(b);
-
-            byte c = 255;
-            sbyte d = (sbyte)c;
-            System.Console.WriteLine(c);
-            System.Console.WriteLine(d);
-
-            uint e = uint.MaxValue; // uint형 최대값 4294967295
-            Console.WriteLine(e);
-            e = e + 1;              // 0000 0000 … … 0000
-            Console.WriteLine(e);
-            
-            int f = int.MaxValue;   // int형 최대값 2147483647
-            Console.WriteLine(f);
-            f = f + 1;              // 1000 0000 … 0000
-            Console.WriteLine(f);
+            CSVar obj = new CSVar();
+            obj.Method1();
         }
     }
 }
